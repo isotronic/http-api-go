4faca023166d99fc9bfb76f40a2719bc8a38c042
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-21 14:45:41 +0000

    feat: implement middleware for file server metrics tracking

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
 func main() {
 	var server http.Server
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
 	server.Addr = ":8080"
 	server.Handler = mux
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middleWareMetricsInt(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		body := fmt.Sprintf("Hits: %v", apiCfg.fileServerHits.Load())
+		w.Write([]byte(body))
+	})
 
 	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) middleWareMetricsInt(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
