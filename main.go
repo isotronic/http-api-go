5e91abf9c38ccc0db82c64220355fe5000e9d192
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-21 12:44:19 +0000

    feat: add health check endpoint and update file server path

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ func main() {
 	server.Addr = ":8080"
 	server.Handler = mux
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server.ListenAndServe()
 }
