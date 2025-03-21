2178c1a5542623366d43ba10c3fcb4fcb6b3d2a5
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-21 19:11:29 +0000

    feat: update admin endpoints for metrics and reset actions

--- main.go
+++ main.go
@@ -23,13 +23,20 @@ func main() {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(200)
-		body := fmt.Sprintf("Hits: %v", apiCfg.fileServerHits.Load())
+		body := fmt.Sprintf(`
+			<html>
+  			<body>
+    			<h1>Welcome, Chirpy Admin</h1>
+    			<p>Chirpy has been visited %d times!</p>
+  			</body>
+			</html>`, 
+			apiCfg.fileServerHits.Load())
 		w.Write([]byte(body))
 	})
-	mux.Handle("POST /api/reset", apiCfg.middleWareMetricsReset(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("POST /admin/reset", apiCfg.middleWareMetricsReset(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 	})))
 
