6afa2b3cfaab5d4915490eb44ce4acc93aba1bbf
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-22 09:22:03 +0000

    feat: add user creation functionality with PostgreSQL integration

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/isotronic/http-go-server/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	database *database.Queries
 }
 
 func main() {
+	godotenv.Load()
 	var server http.Server
 	var apiCfg apiConfig
 	mux := http.NewServeMux()
 	server.Addr = ":8080"
 	server.Handler = mux
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatalf("DB_URL must be set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	apiCfg.database = database.New(db)
 
 	mux.Handle("/app/", apiCfg.middleWareMetricsInt(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", apiHealthzHandler)
