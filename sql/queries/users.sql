23b5ce25f56f56caad97fd050ee5ccac05f9e756
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-24 08:53:06 +0000

    feat: add is_chirpy_red field to user queries and add upgrade functionality

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -12,5 +12,11 @@ RETURNING *;
 -- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1;
 
+-- name: UpgradeUserToChirpyRed :one
+UPDATE users
+SET is_chirpy_red = TRUE
+WHERE id = $1
+RETURNING *;
+
 -- name: ResetUsers :exec
 DELETE FROM users;
